1dfcb67b1311b935eec9c1a85f8afbdfbdeccb9f
michael goboola <raniahgobz@outlook.com>
2022-12-21 12:29:35 +0000

    change the algorithm

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,13 +3,13 @@
 """
 Module 0-pascal_triangle
 """
-
+from math import factorial
 
 def pascal_triangle(n):
     """
     returns a list of integers
     representing Pascal's triangle
-    """
+    
     if n <= 0:
         return []
 
@@ -31,3 +31,18 @@ def pascal_triangle(n):
         # add the row to the triangle
         triangle.append(row)
     return triangle
+"""
+    for i in range(n):
+        for j in range(n-i+1):
+ 
+        # for left spacing
+            print(end=" ")
+ 
+        for j in range(i+1):
+ 
+        # nCr = n!/((n-r)!*r!)
+            print(factorial(i)//(factorial(j)*factorial(i-j)), end=" ")
+ 
+        # for new line
+        print()
+
