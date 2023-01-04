7ced7e8011111092c102ec46c6d723dd8ce4da89
michael goboola <raniahgobz@outlook.com>
2023-01-04 11:41:43 +0000

    local box setup

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,11 @@
+"""
+The localboxes algorithm
+
+"""
+
+def canUnlockAll(boxes):
+    """
+    @params: boxes list of lists (set representation of boxes)
+    Return: True if all can be opened and the reverse is true
+    """
+    pass
---
+++ 0x01-lockboxes/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Test the Algorithm
+"""
+canUnlockAll = __import__('0-lockboxes').canUnlockAll
+
+
+if __name__ == "__main__":
+    boxes = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes))
