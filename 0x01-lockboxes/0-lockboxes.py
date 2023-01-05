f7fdd67ce043eed4161d132f9498320e3cc961de
michael goboola <raniahgobz@outlook.com>
2023-01-05 18:15:03 +0000

    the lockboax algothrim

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,5 @@
 """
 The localboxes algorithm
-
 """
 
 def canUnlockAll(boxes):
@@ -8,4 +7,16 @@ def canUnlockAll(boxes):
     @params: boxes list of lists (set representation of boxes)
     Return: True if all can be opened and the reverse is true
     """
-    pass
+    position = 0
+    unlocked = {}
+
+    for box in boxes:
+        if len(box) == 0 or position == 0:
+            unlocked[position] = "always_unlocked"
+        for key in box:
+            if key < len(boxes) and key != position:
+                unlocked[key] = key
+        if len(unlocked) == len(boxes):
+            return True
+        position += 1
+    return False
