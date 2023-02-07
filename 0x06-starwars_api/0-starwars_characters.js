5490f12968d15b7e835748f63bbb706a7c870773
michaelgobz <raniahgobz@outlook.com>
2023-02-07 12:50:12 +0000

    updated the script

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,16 +1,19 @@
 #!usr/bin/node
 const request = require('request');
-
 /**
- * 
+ *  @param argv
+ *  @returns string
  */
+
 let arg = process.argv.slice(2);
 let url = 'https://swapi-api.alx-tools.com/api/films/'+ arg[0];
-request.get(url, function (erro, res, body) {
+request.get(url, function (err, req, body) {
+    console.error({"error": err })
     let data  = JSON.parse(body)
     data.characters.forEach(character => {
-        let people = []
+        let people = [] // incase of the list 
         request(character, function (err, req, person) {
+            console.error({"error": err })
             let person_data = JSON.parse(person)
             console.log(person_data.name)
         })
