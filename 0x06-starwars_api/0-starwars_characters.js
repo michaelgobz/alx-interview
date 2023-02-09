d2e40da8bfd8aa0c6d642cee3f0a55d20197004f
michaelgobz <raniahgobz@outlook.com>
2023-02-09 15:01:01 +0000

    star wars project

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,62 +1,27 @@
 #!/usr/bin/node
+
 const request = require('request');
-/**
- *  @param argv
- *  @returns string
- */
 
-let arg = process.argv.slice(2);
-let url = 'https://swapi-api.alx-tools.com/api/films/'+ arg[0];
-
-let characters =[];
-let characterNames =[];
-
-/**
- * uses the promises api
- */
-
-const getCharacters =  async () => {
-    await new Promise( resolve => request(url , (err, res, body) => {
-        if (err || res.statusCode !== 200) {
-            console.error('error', err, '| statusCode' , res.statusCode)
-        }else {
-            let chars = JSON.parse(body);
-          characters = chars.characters;
-          resolve();
-      }
-    }))
-};
-
-const getCharactersNames = async () => {
-    if (characters.length > 0) {
-        for (let character in characters) {
-            await new Promise ( resolve => request(character, (err, res, body) => {
-                if (err || res.statusCode !== 200) {
-                    console.error('error', err ,'| statusCode' , res.statusCode)
-                }else {
-                  let person = JSON.parse(body);
-                  characterNames.push(person.name);
-                  resolve();
-                }
-            }))
-        }
+const req = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
     } else {
-        console.error('Error: Got no Characters for check your internet connection other');
+      console.log(JSON.parse(body).name);
+      req(arr, i + 1);
     }
+  });
 };
 
-const getNames = async () => {
-    await getCharacters()
-    await getCharactersNames()
-
-    //print the names
-    for(let charName in characterNames) {
-        if (charName === characterNames[characterNames.length - 1]){
-            process.stdout.write(charName);
-        }else{
-            process.stdout.write(charName + '\n');
-        }
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      const chars = JSON.parse(body).characters;
+      req(chars, 0);
     }
-};
-
-getNames();
+  }
+);
