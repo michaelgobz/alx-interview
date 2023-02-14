f5a4dbe5f74744268c0c781fae14c47909af5d3c
michaelgobz <raniahgobz@outlook.com>
2023-02-14 11:08:22 +0000

    works but not in place

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -8,6 +8,8 @@ def rotate_2d_matrix(matrix):
     '''rotates a 2d matrix 90° clockwise
     Returns: Nothing'''
     left, right = 0, len(matrix) - 1
+    
+    # it works but not in place Transpostion which makes the checker fails
 
     while left < right:
         for i in range(right - left):
