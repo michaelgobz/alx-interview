1919b5a8b6f03fa813a6f86e288ce8abab28b299
Michael Goboola <47690301+michaelgobz@users.noreply.github.com>
2023-02-23 21:36:30 +0000

    so much indents

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,5 +9,25 @@ def makeChange(coinns, total):
         return 0 
         if coins == [] or coins is None: 
             return -1 
-            try: n = coins.index(total) return 1 except ValueError: pass coins.sort(reverse=True) coin_count = 0 for i in coins: if total % i == 0: coin_count += int(total / i) return coin_count if total - i >= 0: if int(total / i) > 1: coin_count += int(total / i) total = total % i else: coin_count += 1 total -= i if total == 0: break if total > 0: return -1 return coin_count
+            try: 
+                n = coins.index(total) 
+                return 1 
+            except ValueError: 
+                pass coins.sort(reverse=True) 
+                coin_count = 0 
+                for i in coins:
+                    if total % i == 0:
+                         coin_count += int(total / i) 
+                         return coin_count
+                         if total - i >= 0: 
+                            if int(total / i) > 1:
+                                 coin_count += int(total / i) 
+                                 total = total % i 
+                            else:
+                                 coin_count += 1 total -= i
+                                 if total == 0: 
+                                    break 
+                                 if total > 0: 
+                                    return -1 
+                        return coin_count
 
